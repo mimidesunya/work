be7b08d1a2c1eee47ecd311453863c214fd8091f
mimi <miyabe.tatsuhiko@gmail.com>
2021-11-05 06:59:26 +0000

    bubble-sort

--- sort/bubble-sort.c
+++ sort/bubble-sort.c
@@ -3,14 +3,18 @@
 #include <string.h>
 #include <stdbool.h>
 
-void bubble_sort(char **list, int size) {
-	char *tmp;
+typedef struct {
+	char* str;
+	size_t pos;
+} entry_t;
+
+void bubble_sort(entry_t *list, int size) {
 	bool swapped;
 	do {
 		swapped = false;
 		for (int i = 1; i < size; ++i) { 
-			if (strcmp(list[i - 1], list[i]) > 0) {
-				tmp = list[i - 1];
+			if (strcmp(list[i - 1].str, list[i].str) > 0) {
+				entry_t tmp = list[i - 1];
 				list[i - 1] = list[i];
 				list[i] = tmp;
 				swapped = true;
@@ -27,30 +31,26 @@ void main(int argc, const char *argv[]) {
 	if (!data) {
 		return;
 	}
-	char **list = (char**)malloc(30000000L);
+	entry_t *list = (entry_t*)malloc(sizeof(entry_t) * 30000000L);
 	if (!list) {
 		return;
 	}
-	char line[1000];
+	char line[700];
 	FILE *fp = fopen(file, "r");
 	char *pdata = data;
 	size_t i = 0;
+	size_t pos = 0;
 	while (fgets(line, sizeof line, fp)) {
-		int len = strlen(line);
-		if (len >= 1000) {
-			break;
-		}
 		char *comma = strchr(line, ',') + 1;
 		char *comma2 = strchr(comma, ',');
 		*comma2 = 0;
 		strcpy(pdata, comma);
-		list[i] = pdata;
-		pdata += len + 1;
+		list[i].str = pdata;
+		list[i].pos = pos;
+		pdata += comma2 - comma + 1;
 		i++;
-		if ((pdata - data) >= 100L * 30000000L) {
-			return;
-		}
-		if (i >= 10000) {
+		pos = ftell(fp);
+		if (i > 100000) {
 			break;
 		}
 	}
@@ -58,10 +58,11 @@ void main(int argc, const char *argv[]) {
 	
 	printf("Sorting ... \n");
 	bubble_sort(list, i);
+	printf("Output ... \n");
 
 	FILE *ip = fopen(index, "w");
 	for (int j = 0; j < i; ++j) {
-		fprintf(ip, "%s\n", list[j]);
+		fprintf(ip, "%010ld %-200s\n", list[j].pos, list[j].str);
 	}
 	fclose(ip);
 
