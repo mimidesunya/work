6e80704ba082ecfeab220ac38b5240b65a38da4e
MIYABE Tatsuhiko <tatsuhiko@miya.be>
2022-09-18 07:44:39 +0000

    sa-is

--- fts/sa-is/sitep4-lms-substr.c
+++ fts/sa-is/sitep4-lms-substr.c
@@ -13,14 +13,14 @@ typedef struct suffix_array {
 // 誘導整列
 void induced_sort(
 	const uint32_t* str,
-	suffix_array_t* sa, const int len,
-	const suffix_array_t* lmsa, const int mcount,
-	const uint32_t* buckets, const int blen) {
+	suffix_array_t* sa, const uint32_t len,
+	const suffix_array_t* lmsa, const uint32_t mcount,
+	const uint32_t* buckets, const uint32_t blen) {
 	// バケットごとの底辺を求める
 	uint32_t* bucketBottoms = malloc(sizeof(uint32_t) * blen);
 	{
-		int j = 0;
-		for (int i = 0; i < blen; ++i) {
+		uint32_t j = 0;
+		for (uint32_t i = 0; i < blen; ++i) {
 			j += buckets[i];
 			bucketBottoms[i] = j - 1;
 		}
@@ -31,14 +31,14 @@ void induced_sort(
 	sa = malloc(sizeof(suffix_array_t) * len);
 	memset(sa, 0, sizeof(suffix_array_t) * len);
 	// LMSだけを埋める
-	for (int i = mcount - 1; i >= 0; --i) {
+	for (uint32_t i = mcount - 1; i != -1; --i) {
 		sa[bucketBottoms[str[lmsa[i].pos]]--] = lmsa[i];
 	}
 	// バケットごとの上辺と底辺を求める
 	uint32_t* bucketTops = malloc(sizeof(uint32_t) * blen);
 	{
-		int j = 0;
-		for (int i = 0; i < blen; ++i) {
+		uint32_t j = 0;
+		for (uint32_t i = 0; i < blen; ++i) {
 			bucketTops[i] = j;
 			j += buckets[i];
 			bucketBottoms[i] = j - 1;
@@ -46,19 +46,19 @@ void induced_sort(
 	}
 	
 	// Lを埋める
-	for (int i = 0; i < len; ++i) {
+	for (uint32_t i = 0; i < len; ++i) {
 		if (sa[i].type != 0 && sa[i].pos > 0 && orgsa[sa[i].pos - 1].type == 'L') {
 			sa[bucketTops[str[sa[i].pos - 1]]++] = orgsa[sa[i].pos - 1];
 		}
 	}
 	// Mを消す
-	for (int i = 0; i < len; ++i) {
+	for (uint32_t i = 0; i < len; ++i) {
 		if (sa[i].type == 'M') {
 			sa[i].type = 0;
 		}
 	}
 	// Sを埋める
-	for (int i = len - 1; i >= 0; --i) {
+	for (uint32_t i = len - 1; i != -1; --i) {
 		if (sa[i].type != 0 && sa[i].pos > 0 && (orgsa[sa[i].pos - 1].type == 'S' || orgsa[sa[i].pos - 1].type == 'M')) {
 			sa[bucketBottoms[str[sa[i].pos - 1]]--] = orgsa[sa[i].pos - 1];
 		}
@@ -71,7 +71,7 @@ void induced_sort(
 }
 
 // 接尾辞配列誘導整列
-void sa_is(suffix_array_t* sa, const uint32_t* str, const int len, const int blen) {
+void sa_is(suffix_array_t* sa, const uint32_t* str, const uint32_t len, const uint32_t blen) {
 	// バケットごとに計数
 	uint32_t* buckets = malloc(sizeof(uint32_t) * blen);
 	memset(buckets, 0, sizeof(uint32_t) * blen);
@@ -81,7 +81,7 @@ void sa_is(suffix_array_t* sa, const uint32_t* str, const int len, const int ble
 	sa[len - 1].type = 'S';
 	int mcount = 0;
 	buckets[str[len - 1]]++;
-	for (int i = len - 2; i >= 0; --i) {
+	for (uint32_t i = len - 2; i != -1; --i) {
 		sa[i].pos = i;
 		if (str[i] == str[i + 1]) {
 			sa[i].type = sa[i + 1].type;
@@ -100,8 +100,8 @@ void sa_is(suffix_array_t* sa, const uint32_t* str, const int len, const int ble
 	suffix_array_t* lmsa = malloc(sizeof(suffix_array_t) * mcount);
 	uint32_t* lmspos = malloc(sizeof(uint32_t) * len);
 	{
-		int j = 0;
-		for (int i = 0; i < len; ++i) {
+		uint32_t j = 0;
+		for (uint32_t i = 0; i < len; ++i) {
 			if (sa[i].type == 'M') {
 				lmspos[sa[i].pos] = j;
 				lmsa[j++] = sa[i];
@@ -117,12 +117,12 @@ void sa_is(suffix_array_t* sa, const uint32_t* str, const int len, const int ble
 	// LMSの順序
 	uint32_t* lmsstr = malloc(sizeof(uint32_t) * mcount + 1);
 	printf("整列後LMS部分文字列\n");
-	int msort = 0;
+	uint32_t msort = 0;
 	{
-		int ppos = 0, plen = 0;
-		for (int i = 0; i < len; ++i) {
+		uint32_t ppos = 0, plen = 0;
+		for (uint32_t i = 0; i < len; ++i) {
 			if (sa[i].type == 'M') {
-				int lpos = lmspos[sa[i].pos];
+				uint32_t lpos = lmspos[sa[i].pos];
 				uint32_t llen = (lpos == mcount - 1) ? 1 : lmsa[lpos + 1].pos - lmsa[lpos].pos + 1;
 				if (llen != plen ||
 					memcmp(str + lmsa[lpos].pos, str + lmsa[ppos].pos, sizeof(uint32_t) * llen) != 0) {
@@ -149,7 +149,7 @@ void sa_is(suffix_array_t* sa, const uint32_t* str, const int len, const int ble
 	
 	// LMS部分文字列
 	printf("LMS部分文字列\n");
-	for (int i = 0; i < mcount; ++i) {
+	for (uint32_t i = 0; i < mcount; ++i) {
 		uint32_t llen = (i == mcount - 1) ? 1 : lmsa[i + 1].pos - lmsa[i].pos + 1;
 		printf("%2d %2d ", i, lmsstr[i]);
 		for (uint32_t j = lmsa[i].pos; j < lmsa[i].pos + llen; ++j) {
@@ -168,19 +168,19 @@ void sa_is(suffix_array_t* sa, const uint32_t* str, const int len, const int ble
 	// LMS部分文字列がユニークであることが終了条件
 	if (msort != mcount) {
 		// lmsstrに対して再帰処理
-		int rlen = mcount + 1;
+		uint32_t rlen = mcount + 1;
 		suffix_array_t* rsa = malloc(sizeof(suffix_array_t) * rlen);
 		lmsstr[mcount] = 0;
 		sa_is(rsa, lmsstr, rlen, msort + 1);
 		
 		// 再整列したLMS
 		suffix_array_t* rlmsa = malloc(sizeof(suffix_array_t) * mcount);
-		for (int i = 1; i < rlen; ++i) {
+		for (uint32_t i = 1; i < rlen; ++i) {
 			rlmsa[i - 1] = lmsa[rsa[i].pos];
 		}
 		free(lmsa);
 		printf("整列済みLMS\n");
-		for (int i = 0; i < mcount; ++i) {
+		for (uint32_t i = 0; i < mcount; ++i) {
 			printf("%2d %s\n", rlmsa[i].pos, STRING + rlmsa[i].pos);
 		}
 		lmsa = rlmsa;
@@ -197,9 +197,9 @@ void sa_is(suffix_array_t* sa, const uint32_t* str, const int len, const int ble
 }
 
 void main(int argc, const char *argv[]) {
-	const int len = strlen(STRING) + 1;
+	const uint32_t len = strlen(STRING) + 1;
 	uint32_t* str = malloc(sizeof(uint32_t) * len);
-	for (int i = 0; i < len; ++i) {
+	for (uint32_t i = 0; i < len; ++i) {
 		str[i] = (unsigned char)STRING[i];
 	}
 	suffix_array_t* sa = malloc(sizeof(suffix_array_t) * len);
@@ -207,7 +207,7 @@ void main(int argc, const char *argv[]) {
 	
 	// 表示
 	printf("整列済み接尾辞配列\n");
-	for (int i = 0; i < len; ++i) {
+	for (uint32_t i = 0; i < len; ++i) {
 		char* type = "   ";
 		switch(sa[i].type) {
 			case 0:
